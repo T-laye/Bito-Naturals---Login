df3bd41822d581df0a13fd0c2c082663a7767ec8
Laye <tlayefaah@gmail.com>
2022-10-24 14:46:42 +0100

    build:responsive

--- js/login.js
+++ js/login.js
@@ -7,12 +7,50 @@ const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const form = document.querySelector(".form-container");
 
-signUpButton.addEventListener("click", () =>
-  container.classList.add("right-panel-active")
-);
+// query//
+const queryContainer = document.querySelector(".query-container");
+const signContainer = document.querySelector(".sign-inpage");
+const createBtn = document.querySelector(".create-btn");
+const helloFriend = document.querySelector(".hello-friend");
+const createForm = document.querySelector(".create-account");
+const mobileSignUp = document.querySelector(".mobile-sign-up");
+const welcomePage = document.querySelector(".welcome-page");
+const logBtn = document.querySelector(".log-btn");
+const welcomeBtn = document.querySelector(".welcome-btn");
+
+signUpButton.addEventListener("click", function () {
+  console.log("gukguk");
+  container.classList.add("right-panel-active");
+});
 
 signInButton.addEventListener("click", function () {
   container.classList.remove("right-panel-active");
 });
 
 // console.log(form);
+
+createBtn.addEventListener("click", function () {
+  console.log("clicked");
+
+  signContainer.classList.add("overlay-top");
+  helloFriend.classList.add("overlay-bottom");
+});
+
+mobileSignUp.addEventListener("click", function () {
+  console.log("clickeddddddddddddd");
+  createForm.classList.add("overlay-bottom--signup");
+});
+
+logBtn.addEventListener("click", function () {
+  console.log("gerroutttttt");
+  welcomePage.classList.add("overlay-bottom--welcome");
+});
+
+welcomeBtn.addEventListener("click", function () {
+  console.log("xcghvhtttt");
+
+  welcomePage.classList.remove("overlay-bottom--welcome");
+  helloFriend.classList.remove("overlay-bottom");
+  createForm.classList.remove("overlay-bottom--signup");
+  // welcomePage.classList.add("overlay-signin");
+});
